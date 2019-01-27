dbeb8c67ba6b1c3cf5e1f932db654b4310ff983d
Luke Williams <luke@lukewilliamsmusic.com>
2019-01-27 11:58:14 -0500

    first web upload, ready to show ian

--- js/behind-sketch.js
+++ js/behind-sketch.js
@@ -4,102 +4,185 @@ var glass;
 var halfWidth, halfHeight;
 var rightEdge, leftEdge;
 
+var vidSize;
+
 var iconPositions = [];
 
-var containerWidth;
+var containerHalfWidth;
 
 var icons = [];
 
 var anAppIsActive;
 
-var musicApp;
+var musicApp, messagesApp, mediaApp, calendarApp;
+var socialRow;
 var apps = [];
 
+var homeButton;
+
+var d, minutes, hours;
+var statusTime;
+
+var painting1, painting2;
+
 function preload() {
+    painting1 = loadImage('./img/painting4.png');
+    painting2 = loadImage('./img/painting5.png');
 }
 
 function setup() {
+
     var canv = createCanvas(windowWidth, windowHeight);
     canv.parent('canv');
-    vid = createVideo('img/thumb-test.mov');
+
+    doTime();
+
+    vid = createVideo('img/loop.mp4');
     vid.volume(0);
+    vid.elt.muted = true;
     vid.loop();
     vid.hide();
-    containerWidth = 300;
+    if (height > 600){
+        vidSize = new p5.Vector(height*1.2, height);
+    } else {
+        vidSize = new p5.Vector(720, 600);
+    }
+
+    if (width > 600){
+        containerHalfWidth = 300;
+    } else {
+        containerHalfWidth = width/2;
+    }
     halfWidth = width/2;
     halfHeight = height/2;
-    rightEdge = halfWidth + 300;
-    leftEdge = halfWidth - 300;
+    rightEdge = halfWidth + containerHalfWidth;
+    leftEdge = halfWidth - containerHalfWidth;
 
     anAppIsActive = false;
 
-    iconPositions[0] = new p5.Vector (rightEdge - 90, 90);
-    iconPositions[1] = new p5.Vector (rightEdge - 230, 90);
-    iconPositions[2] = new p5.Vector (rightEdge - 370, 90);
-    iconPositions[3] = new p5.Vector (rightEdge - 510, 90);
-    iconPositions[4] = new p5.Vector (rightEdge - 90, 230);
-    iconPositions[5] = new p5.Vector (rightEdge - 230, 230);
-    iconPositions[6] = new p5.Vector (rightEdge - 370, 230);
-    for (i=0;i<iconPositions.length;i++){
-        icons[i] = new Icon(iconPositions[i].x, iconPositions[i].y);
+    positions();
+    iconPositions[0] = new p5.Vector (rightEdge - containerHalfWidth*0.3, containerHalfWidth*0.4);
+    iconPositions[1] = new p5.Vector (rightEdge - containerHalfWidth*0.77, containerHalfWidth*0.4);
+    iconPositions[2] = new p5.Vector (leftEdge + containerHalfWidth*0.77, containerHalfWidth*0.4);
+    iconPositions[3] = new p5.Vector (leftEdge + containerHalfWidth*0.3, containerHalfWidth*0.4);
+    iconPositions[4] = new p5.Vector (rightEdge - containerHalfWidth*0.3, containerHalfWidth*0.87);
+    iconPositions[5] = new p5.Vector (rightEdge - containerHalfWidth*0.77, containerHalfWidth*0.87);
+    iconPositions[6] = new p5.Vector (leftEdge + containerHalfWidth*0.77, containerHalfWidth*0.87);
+    for (i=0;i<4;i++){
+        icons[i] = new Icon(iconPositions[i].x, iconPositions[i].y, i);
+        // if (width > 600){
+        //     icons[i].smallSize = new p5.Vector (100, 100);
+        // } else {
+        //     icons[i].smallSize = new p5.Vector (width/6, width/6);
+        // }
     }
+    sizes();
+    
 
     musicApp = select('.music');
+    messagesApp = select('.messages');
+    mediaApp = select('.media');
+    calendarApp = select('.calendar');
+    socialRow = select('.social-media-row');
     apps[0] = musicApp;
+    apps[1] = messagesApp;
+    apps[2] = mediaApp;
+    apps[3] = calendarApp;
+
+    homeButton = select('.home-button');
+    homeButton.mousePressed(homeButtonPressed);
 }
 
 function draw() {
     strokeWeight(5);
     imageMode(CENTER);
-    image(vid, halfWidth, halfHeight, height*1.2, height)
-
-    
+    image(vid, halfWidth, halfHeight, vidSize.x, vidSize.y);
 
     for (i=0;i<icons.length;i++){
         icons[i].show();
     }
 
+    positions();
+    sizes();
+
+    setInterval(doTime, 1000);
+
     phoneFrame();
+    backgroundPainting();
 }
 
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight);
-    halfWidth = width/2;
-    halfHeight = height/2;
-    rightEdge = halfWidth + 300;
-    leftEdge = halfWidth - 300;
+    
 
-    // icons are resized & re-positioned
-    iconPositions[0] = new p5.Vector (rightEdge - 90, 90);
-    iconPositions[1] = new p5.Vector (rightEdge - 230, 90);
-    iconPositions[2] = new p5.Vector (rightEdge - 370, 90);
-    iconPositions[3] = new p5.Vector (rightEdge - 510, 90);
-    iconPositions[4] = new p5.Vector (rightEdge - 90, 230);
-    iconPositions[5] = new p5.Vector (rightEdge - 230, 230);
-    iconPositions[6] = new p5.Vector (rightEdge - 370, 230);
+}
+
+function positions(){
+    halfWidth = width*0.5;
+    halfHeight = height*0.5;
+    rightEdge = halfWidth + containerHalfWidth;
+    leftEdge = halfWidth - containerHalfWidth;
+    var dist1 = int(containerHalfWidth*0.3);
+    var dist2 = int(containerHalfWidth*0.77);
+    var dist3 = 40 + int(containerHalfWidth*0.3);
+    var dist4 = 40 + int(containerHalfWidth*0.77);
+    
+    console.log(dist1, dist2, dist3, dist4);
+
+    if (windowWidth > 600){
+        containerHalfWidth = 300;
+    } else {
+        containerHalfWidth = width*0.5;
+    }
+
+    // icons are re-positioned
+    iconPositions[0] = new p5.Vector (rightEdge - dist1, dist3);
+    iconPositions[1] = new p5.Vector (rightEdge - dist2, dist3);
+    iconPositions[2] = new p5.Vector (leftEdge + dist2, dist3);
+    iconPositions[3] = new p5.Vector (leftEdge + dist1, dist3);
+    iconPositions[4] = new p5.Vector (rightEdge - dist1, dist4);
+    iconPositions[5] = new p5.Vector (rightEdge - dist2, dist4);
+    iconPositions[6] = new p5.Vector (leftEdge + dist2, dist4);
+
+    
+}
+
+function sizes(){
+    // icons are re-sized
+    var smallSize = int(width*0.166);
+
     for (i=0;i<icons.length;i++){
         if (width > 600){
             icons[i].smallSize = new p5.Vector (100, 100);
         } else {
-            icons[i].smallSize = new p5.Vector (100, 100);
+            icons[i].smallSize = new p5.Vector (smallSize, smallSize);
         }
 
+        icons[i].bigSize = new p5.Vector (containerHalfWidth*2, height);
+        icons[i].rounded = icons[i].smallSize.x*0.2;
         icons[i].initialPos = iconPositions[i];
         icons[i].centerPos = new p5.Vector(halfWidth, halfHeight);
         
     }
 
+    if (height > 600){
+        vidSize = new p5.Vector(height*1.2, height);
+    } else {
+        vidSize = new p5.Vector(720, 600);
+    }
 }
 
 function phoneFrame(){
     //BLACK RECTANGLES
     push();
         rectMode(CORNER);
-        fill(0,0,0);
+        fill(1,3,15);
         noStroke();
-        rect(0, 0, halfWidth - containerWidth, height);
-        rect(halfWidth + containerWidth, 0, halfWidth - containerWidth, height);
-        rect(halfWidth + containerWidth, 0, halfWidth - containerWidth, height);
+        rect(0, 0, halfWidth - containerHalfWidth, height);
+        fill(1,5,9);
+        rect(halfWidth + containerHalfWidth, 0, halfWidth - containerHalfWidth, height);
+        fill(3,3,12);
+        rect(halfWidth -containerHalfWidth, 0, halfWidth + containerHalfWidth, 20);
     pop();
 
     //STATUS BAR
@@ -107,39 +190,70 @@ function phoneFrame(){
         rectMode(CORNER);
         fill(255,255,255, 210);
         noStroke();
-        rect(leftEdge, 0, containerWidth*2, 15);
-    pop();
-
-    push();
-        rectMode(CORNER);
-        fill(255,255,255, 210);
-        noStroke();
-        rect(leftEdge, 0, containerWidth*2, 15);
+        rect(leftEdge, 20, containerHalfWidth*2, 25);
     pop();
 
     //HOME BUTTON
 
-    push();
-        rectMode(CENTER);
-        fill(0,0,0, 210);
-        noStroke();
-        ellipse(halfWidth, height - 50, 80, 80);
-    pop();
+    // push();
+    //     rectMode(CENTER);
+    //     fill(0,0,0, 210);
+    //     noStroke();
+    //     ellipse(halfWidth, height - 50, 80, 80);
+    // pop();
 
     //ROUNDED CORNERS OF PHONE
 
     // push();
     //     rectMode(CENTER);
+    //     fill(0,0,0);
+    //     noStroke();
+    //     rect(halfWidth, height-50, containerHalfWidth*2, 100);
+    // pop();
+    // push();
+    //     rectMode(CENTER);
     //     noFill();
     //     stroke(0, 0, 0);
-    //     rect(halfWidth, halfHeight, containerWidth*2, height, 50);
+    //     strokeWeight(20);
+    //     rect(halfWidth, halfHeight, containerHalfWidth*2, height, 50);
     // pop();
-    
+}
+
+function backgroundPainting(){
+    var leftWidth = ((width - (containerHalfWidth*2))/2);
+    var rightPos = ((width*0.5 + (containerHalfWidth)));
+    if (width > 600){
+        push();
+            imageMode(CORNER);
+            image(painting1, 0, 0, leftWidth, height);
+            image(painting2, rightPos, 0, leftWidth, height);
+        pop();
+    }
+}
+
+function touchStarted(){
+    for (i=0;i<icons.length;i++){
+        if (touches[0].x > icons[i].initialPos.x - icons[i].smallSize.x*0.5 && touches[0].x < icons[i].initialPos.x + icons[i].smallSize.x*0.5 && touches[0].y > icons[i].initialPos.y - icons[i].smallSize.y*0.5 && touches[0].y < icons[i].initialPos.y + icons[i].smallSize.y*0.5){
+            icons[i].touched = true;
+        } else {
+            icons[i].touched = false;
+        }
+        if (icons[i].touched){
+            if (!icons[i].active && !anAppIsActive){
+                icons[i].active = true;
+                anAppIsActive = true;
+                socialRow.style("transform: scale(0); opacity: 0");
+                //SHOW CONTENT
+                if (apps[i] != undefined){
+                    apps[i].style('transform: scale(1); opacity: 1');
+                }
+            }
+        }
+    }
 }
 
 function mousePressed(){
     
-
     // EXPAND AN APP
     for (i=0;i<icons.length;i++){
         // if you click an icon, it expands
@@ -147,9 +261,10 @@ function mousePressed(){
             if (!icons[i].active && !anAppIsActive){
                 icons[i].active = true;
                 anAppIsActive = true;
+                socialRow.style("transform: scale(0); opacity: 0");
                 //SHOW CONTENT
                 if (apps[i] != undefined){
-                    apps[i].style('display: block');
+                    apps[i].style('transform: scale(1); opacity: 1');
                 }
             }
         
@@ -159,23 +274,43 @@ function mousePressed(){
         }
     }
 
-    // HOME BUTTON
+    // HOME BUTTON HELPER
     if (mouseX > halfWidth - 40 && mouseX < halfWidth + 40 && mouseY > height - 90 && mouseY < height - 10){
-        console.log("yep!");
-        for (i=0;i<icons.length;i++){
-            
-            //HIDE CONTENT
-            if (icons[i].active){
-                if (apps[i] != undefined){
-                    apps[i].style('display: none');
-                }
-            }
-            icons[i].active = false;
-            
-        }
-        anAppIsActive = false;
+
+        homeButtonPressed();
     }
 
+    
+}
+
+function doTime(){
+    d = new Date();
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes();
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours();
+    statusTime = hours + ":" + minutes;
+    document.getElementById('time').innerHTML = statusTime;
+    // statusTime = createElement('h6', hours + ":" + minutes);
+    // statusTime.parent('time');
+}
+
+function expandApp(){
+   
+}
+
+function homeButtonPressed(){
+    
+    for (i=0;i<icons.length;i++){
+        //HIDE CONTENT
+        if (icons[i].active){
+            if (apps[i] != undefined){
+                apps[i].style("transform: translateX(100px); transform: scale(0); opacity: 0");
+            }
+            
+        }
+        icons[i].active = false;
+    }
+    socialRow.style("transform: scale(1); opacity: 1");
+    anAppIsActive = false;
     
 }
 
--- js/icon.js
+++ js/icon.js
@@ -1,12 +1,17 @@
-function Icon(x, y){
+function Icon(x, y, ind){
     this.initialPos = new p5.Vector(x, y);
     this.centerPos = new p5.Vector(halfWidth, halfHeight);
     this.actualPos = this.initialPos;
-    this.smallSize = new p5.Vector(100, 100);
+    if (width > 600){
+        this.smallSize = new p5.Vector(100, 100);
+    } else {
+        this.smallSize = new p5.Vector(int(width*0.166), int(width*0.166));
+    }
     this.goneSize = new p5.Vector(0, 0);
-    this.bigSize = new p5.Vector (containerWidth*2, height);
+    this.bigSize = new p5.Vector (containerHalfWidth*2, height);
     this.actualSize = this.smallSize;
     this.mOver = false;
+    this.touched = false;
     this.active = false;
     this.gone = false;
     this.stateChange = 0;
@@ -14,11 +19,36 @@ function Icon(x, y){
     this.disappearChange = 0;
     this.disappearChangeEased = 0;
     this.easeSpeed = 0.1;
-    this.rounded = 20;
+    this.rounded = this.smallSize.x*0.2;
+    this.ind = ind;
+    this.icon = loadImage('./img/icons/icon' + this.ind + '.png');
+    if (this.ind === 0){
+        this.smallColor = color(255, 255, 255, 200);
+        this.bigColor = color(255, 255, 255, 200);
+        this.color = this.smallColor;
+    } else if (this.ind === 1){
+        this.smallColor = color(84, 228, 77, 200);
+        this.bigColor = color(255, 255, 255, 200);
+        this.color = this.smallColor;
+    } else if (this.ind === 2){
+        this.smallColor = color(170, 170, 170, 200);
+        this.bigColor = color(255, 255, 255, 200);
+        this.color = this.smallColor;
+    } else if (this.ind === 3){
+        this.smallColor = color(255, 255, 255, 200);
+        this.bigColor = color(255, 255, 255, 200);
+        this.color = this.smallColor;
+    } else {
+        this.smallColor = color(int(random(0, 255)), int(random(0, 255)), int(random(0, 255)), 200);
+        this.bigColor = color(int(random(0, 255)), int(random(0, 255)), int(random(0, 255)), 200);
+        this.color = this.smallColor;
+    }
+    
 
     this.show = function(){
+        push();
         rectMode(CENTER);
-        fill(255, 255, 255, 200);
+        fill(this.color);
         noStroke();
         rect(this.actualPos.x, this.actualPos.y, this.actualSize.x, this.actualSize.y, this.rounded);
 
@@ -30,16 +60,20 @@ function Icon(x, y){
 
         if (!this.active && anAppIsActive){
             this.disappear();
+            this.gone = true;
+        } else {
+            this.gone = false;
         }
 
         if (!this.active){
-            if (mouseX > this.actualPos.x - this.actualSize.x*0.5 && mouseX < this.actualPos.x + this.actualSize.x*0.5 && mouseY > this.actualPos.y - this.actualSize.y*0.5 && mouseY < this.actualPos.y + this.actualSize.y*0.5){
+            if (mouseX > this.initialPos.x - this.smallSize.x*0.5 && mouseX < this.initialPos.x + this.smallSize.x*0.5 && mouseY > this.initialPos.y - this.smallSize.y*0.5 && mouseY < this.initialPos.y + this.smallSize.y*0.5){
                 this.mOver = true;
             } else {
                 this.mOver = false;
             }
 
         }
+        pop();
     }
 
     this.shrink = function(){
@@ -49,14 +83,20 @@ function Icon(x, y){
         }
 
         //round the corners
-        if (this.rounded < 20){
+        if (this.rounded < this.smallSize.x * 0.2){
             this.rounded += 2;
         }
-        this.stateChangeEased = this.easeInOutQuad(constrain(this.stateChange, 0, 1));
+        this.stateChangeEased = constrain(this.easeInOutQuad(constrain(this.stateChange, 0, 1)), 0, 1);
 
         this.actualPos = p5.Vector.lerp(this.initialPos, this.centerPos, this.stateChangeEased);
         this.actualSize = p5.Vector.lerp(this.smallSize, this.bigSize, this.stateChangeEased);
-        
+        this.color = lerpColor(this.smallColor, this.bigColor, this.stateChangeEased);
+
+        push();
+        if (!this.gone){
+            image(this.icon, this.initialPos.x, this.initialPos.y, this.smallSize.x, this.smallSize.y);
+        }
+        pop();
     }
 
     this.grow = function(){
@@ -73,13 +113,15 @@ function Icon(x, y){
 
         this.actualPos = p5.Vector.lerp(this.initialPos, this.centerPos, this.stateChangeEased);
         this.actualSize = p5.Vector.lerp(this.smallSize, this.bigSize, this.stateChangeEased);
+        this.color = lerpColor(this.smallColor, this.bigColor, this.stateChangeEased);
+
     }
 
     this.disappear = function(){
             if (this.disappearChange < 1){
                 this.disappearChange += this.easeSpeed;
             }
-            this.disappearChangeEased = this.easeInOutQuad(constrain(this.disappearChange, 0, 1));
+            this.disappearChangeEased = constrain(this.easeInOutQuad(constrain(this.disappearChange, 0, 1)), 0, 1);
             this.actualSize = p5.Vector.lerp(this.smallSize, this.goneSize, this.disappearChangeEased);
     }
 
